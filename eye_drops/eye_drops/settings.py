a48b2d943cb5e200a3fb3cedbde06c05d0c1d5d6
shoichi3 <dece12shou@gmail.com>
2021-09-14 06:21:44 +0900

    SECRET_KEYの設定

--- eye_drops/eye_drops/settings.py
+++ eye_drops/eye_drops/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-bs=rmkju-8s3j40zca3pk21q33s!ukg(*m_$pq%vh#vi2c0+q0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
