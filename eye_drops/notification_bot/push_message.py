c264f58f819c461618f9ff6bc8f1004594cf750e
shoichi3 <dece12shou@gmail.com>
2021-12-31 17:44:26 +0900

    テンプレートメッセージおよび発火タイミングの変更

--- eye_drops/notification_bot/push_message.py
+++ eye_drops/notification_bot/push_message.py
@@ -2,9 +2,9 @@ from linebot import LineBotApi
 from linebot.models import TemplateSendMessage,ConfirmTemplate,MessageAction
 from linebot.exceptions import LineBotApiError
 
-import sys
-sys.path.append('/line_bot/eye_drops/libraries.py')
-import libraries
+#import sys
+#sys.path.append('/line_bot/eye_drops/libraries.py')
+#import libraries
 
 import os
 import json
@@ -14,7 +14,23 @@ USERID = os.environ.get("USERID")
 line_bot_api = LineBotApi(ACCESSTOKEN)
 
 def main():
-  confirm_template_message1 = libraries.push_message(1)
+  confirm_template_message1 = TemplateSendMessage(
+                                alt_text='目薬の時間です',
+                                template=ConfirmTemplate(
+                                    text='1つ目の目薬を打ちましたか？',
+                                    actions=[
+                                        MessageAction(
+                                            label='はい',
+                                            text='はい'
+                                        ),
+                                        MessageAction(
+                                            label='いいえ',
+                                            text='いいえ'
+                                        )
+                                    ]
+                                )
+                            )
+  #confirm_template_message1 = libraries.push_message(1)
   
   try:
     line_bot_api.push_message(USERID, messages=confirm_template_message1)
